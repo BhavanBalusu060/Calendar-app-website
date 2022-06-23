fe80167bd667472ed0023a7e7288e36e5d41637a
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2022-06-23 15:39:49 -0700

    Pushhing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+import Sign from './Components/SignIn';
+import Register from './Components/Register';
+import Dashboard from './Components/Dashboard';
+import Reset from './Components/Reset';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Sign/>}/>;
+        <Route path="/dash" element={<Dashboard/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/reset" element={<Reset/>}/> 
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
